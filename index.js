a3072852aa4df371a068e3893e1742ebc61a74d8
Anderson Chavez Rodrigez <achavezrodrigez7@gmail.com>
2025-06-22 01:51:40 -0500

    three commit

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
         particula.style.left = `${x}px`;
         particula.style.top = `${y}px`;
 
-        // Dirección aleatoria
         const angulo = Math.random() * 2 * Math.PI;
         const distancia = Math.random() * 10;
         particula.style.setProperty('--dx', Math.cos(angulo) * distancia);
@@ -32,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -44,10 +42,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const video = document.getElementById('video-fondo');
     const boton1 = document.getElementById('boton1');
     const boton2 = document.getElementById('boton2');
-    const boton3 = document.getElementById('boton3');
-    const boton4 = document.getElementById('boton4');
+    const invitado = document.getElementById('nombre-invitado');
     let mostrado1 = false;
     let mostrado2 = false;
+    let mostrado3 = false;
 
     boton.addEventListener('click', () => {
         fondo.classList.add('fondo-distorsion');
@@ -57,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const elem = document.documentElement;
         if (elem.requestFullscreen) {
             elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) { // Safari
+        } else if (elem.webkitRequestFullscreen) {
             elem.webkitRequestFullscreen();
         }
         setTimeout(() => {
@@ -86,31 +84,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
         setTimeout(() => {
             video.addEventListener('timeupdate', () => {
-                if (!mostrado1 && video.currentTime >= 21) {
+
+                if(!mostrado3 && video.currentTime >= 15){
+                    invitado.classList.add("visible");
+                    mostrado3 = true;
+                }
+
+                if(mostrado3 && video.currentTime >= 19){
+                    invitado.classList.remove("visible");
+                    mostrado3 = false;
+                }
+
+                if (!mostrado1 && video.currentTime >= 40) {
                     boton1.style.display = 'block';
                     mostrado1 = true
                 }
 
-                if (mostrado1 && video.currentTime >= 26) {
+                if (mostrado1 && video.currentTime >= 46) {
                     boton1.style.display = 'none';
                     mostrado1 = false
                 }
 
-                if (!mostrado2 && video.currentTime >= 26) {
+                if (!mostrado2 && video.currentTime >= 46) {
                     boton2.style.display = 'block';
-                    boton3.style.display = 'block';
-                    boton4.style.display = 'block';
                     mostrado2 = true;
                 }
 
-                if (mostrado2 && video.currentTime >= 32) {
+                if (mostrado2 && video.currentTime >= 50) {
                     boton2.style.display = 'none';
-                    boton3.style.display = 'none';
-                    boton4.style.display = 'none';
                     mostrado2 = false;
                 }
             });
-        }, 30000);
+        }, 28000);
 
     });
 
@@ -144,3 +149,213 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+const invitados = {
+
+    connie: {
+        nombre: "Connie",
+        acompañante: true,
+    },
+
+    omara:{
+        nombre: "Omara",
+        acompañante: false,
+    },
+
+    alexandra:{
+        nombre: "Alexandra",
+        acompañante: true,
+    },
+
+    adriana:{
+        nombre: "Adriana",
+        acompañante: true,
+    },
+
+    belen:{
+        nombre: "Belen",
+        acompañante: false,
+    },
+
+    maria:{
+        nombre: "Maria",
+        acompañante: false,
+    },
+
+    flavia:{
+        nombre: "Flavia",
+        acompañante: false,
+    },
+
+    elkin:{
+        nombre: "Elkin",
+        acompañante: false,
+    },
+
+    josue:{
+        nombre: "Josue",
+        acompañante: false,
+    },
+
+    haroltd:{
+        nombre: "Haroltd",
+        acompañante: false,
+    },
+
+    sharukh:{
+        nombre: "Sharukh",
+        acompañante: true,
+    },
+
+    sergio:{
+        nombre: "Sergio",
+        acompañante: false,
+    },
+
+    sebastian:{
+        nombre: "Sebastian",
+        acompañante: false,
+    },
+
+    ezequiel:{
+        nombre: "Ezequiel",
+        acompañante: false,
+    },
+
+    danilo:{
+        nombre: "Danilo",
+        acompañante: false,
+    },
+
+    facundo:{
+        nombre: "Facundo",
+        acompañante: false,
+    },
+
+    dylan:{
+        nombre: "Dylan",
+        acompañante: false,
+    },
+
+    jhon:{
+        nombre: "Jhon",
+        acompañante: false,
+    },
+
+    flavia2:{
+        nombre: "Flavia",
+        acompañante: true,
+    },
+
+    micaela:{
+        nombre: "Micaela",
+        acompañante: false,
+    },
+
+    ashley:{
+        nombre: "Ashley",
+        acompañante: false,
+    },
+
+    leo:{
+        nombre: "Leo",
+        acompañante: true,
+    },
+
+    edinson:{
+        nombre: "Edinson",
+        acompañante: true,
+    },
+
+    mis_maria:{
+        nombre: "Mis Maria",
+        acompañante: true,
+    },
+
+    mis_miriam:{
+        nombre: "Mis Miriam",
+        acompañante: false,
+    },
+
+    mis_liz:{
+        nombre: "Mis Liz",
+        acompañante: true,
+    },
+
+    mis_ursula:{
+        nombre: "Mis Ursula",
+        acompañante: true,
+    },
+
+    mis_doris:{
+        nombre: "Mis Doris",
+        acompañante: false,
+    },
+
+    tia_estefani:{
+        nombre: "Tía Estefani",
+        acompañante: true,
+    },
+
+    tia_elsa:{
+        nombre: "Tía Elsa",
+        acompañante: true,
+    },
+
+    frank:{
+        nombre: "Frank",
+        acompañante: true,
+    },
+
+    tio_orzon:{
+        nombre: "Tío Orzon",
+        acompañante: false,
+    },
+
+    tio_james:{
+        nombre: "Tío James",
+        acompañante: false,
+    },
+
+    tio_deivis:{
+        nombre: "Tío Deivis",
+        acompañante: true,
+    },
+
+    mamita_dela:{
+        nombre: "Mamita Dela",
+        acompañante: false,
+    },
+
+    papito_sixto:{
+        nombre: "Papito Sixto",
+        acompañante: false,
+    },
+
+    tia_yesenia:{
+        nombre: "Tía Yesenia",
+        acompañante: true,
+    },
+
+    yomira:{
+        nombre: "Yomira",
+        acompañante: true,
+    },
+
+};
+
+const datos = invitados[id];
+
+if(datos) {
+    document.getElementById('nombre-invitado').textContent = datos.nombre;
+
+    const boton = document.getElementById('boton2');
+    if(datos.acompañante) {
+        boton.href= "https://forms.gle/mko7TKSt2t3A8Wxb7";
+    } else {
+        boton.href = "https://forms.gle/s9BpnmmQKSxAVZoJ8";
+    }
+}
